2f0cd5f18b1ef2d8db6270b2413a6afef32b5d0e
sunsfan@gmail.com <sunsfan@gmail.com@37ce4eaa-fe95-99a7-7cfd-c73f05081525>
2011-11-22 03:23:15 +0000

    Updated WPSTab2 (Progress Bars Continuously Update)

--- trunk/WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSTab2.java
+++ trunk/WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSTab2.java
@@ -1,5 +1,8 @@
 package com.breakawaypcsolutions;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,8 +11,8 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class WPSTab2 extends Activity {
-    
+public class WPSTab2 extends Activity{
+	    
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -21,28 +24,42 @@ public class WPSTab2 extends Activity {
  	           Intent myIntent = new Intent(view.getContext(), WPSVideoLibrary.class);
  	           startActivityForResult(myIntent, 0);
  	       }
- 	   });
+ 	   });	
  	   
- 	   // Input Data Table (Event Driven)
+ 	   Button dtTest = (Button) findViewById(R.id.button1);
+ 	   dtTest.setOnClickListener(new View.OnClickListener() {
+ 	       public void onClick(View view) {
+ 	    	    
+ 	    	  TimerTask task = new TimerTask()
+ 	    	  {
+				@Override
+				public void run() {
+	    	   			// Generate Data Table
+	    	   			int pb1 = (int)(Math.random()*100);
+	    	   			int pb2 = (int)(Math.random()*100);
+	    	   			int pb3 = (int)(Math.random()*100);
+	    	   			int pb4 = (int)(Math.random()*100);
+	      		
+	    	   			// Update Progress Bars
+	    	   			ProgressBar pbFeedPump = (ProgressBar) findViewById(R.id.progressBar1);
+	    	   			ProgressBar pbMediaFilter = (ProgressBar) findViewById(R.id.progressBar2);
+	    	   			ProgressBar pbCartridges = (ProgressBar) findViewById(R.id.progressBar3);
+	    	   			ProgressBar pbMotor = (ProgressBar) findViewById(R.id.progressBar4);
+	      		   
+	    	   			// Update Progress Bars
+	    	   			pbFeedPump.setProgress(pb1);
+	    	   			pbMediaFilter.setProgress(pb2);
+	    	   			pbCartridges.setProgress(pb3);
+	    	   			pbMotor.setProgress(pb4);		
+				} 
+ 	    	  };
+ 	    	  
+	    	  new Timer().scheduleAtFixedRate(task, 2000, 2000);
+ 	    	  
+ 	       }
+ 	    	  
+ 	   });	
  	   
- 	   // Save Data Locally (Necessary ???)
- 	   
- 	   // Interpret Data
- 	   
- 	   // Update Progress Bars
- 	   ProgressBar pbFeedPump = (ProgressBar) findViewById(R.id.progressBar1);
- 	   pbFeedPump.setProgress(25);
- 	   
- 	   ProgressBar pbMediaFilter = (ProgressBar) findViewById(R.id.progressBar2);
-	   pbMediaFilter.setProgress(50);
-	   
-	   ProgressBar pbCartridges = (ProgressBar) findViewById(R.id.progressBar3);
- 	   pbCartridges.setProgress(75);
- 	   
- 	   ProgressBar pbMotor = (ProgressBar) findViewById(R.id.progressBar4);
-	   pbMotor.setProgress(100);
-	   
-	   // Output Data Table to Controllers
- 	   
-    }
-}
+	}
+
+	}
