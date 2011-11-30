990b20e8fa21ae6570b2369361d29982856425bc
alex.neumann07 <alex.neumann07@gmail.com>
2011-11-30 19:34:13 +0000

    changed layout to include text view with color on top and no tabs

--- WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSPrototypeActivity.java
+++ WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSPrototypeActivity.java
@@ -1,39 +1,171 @@
 package com.breakawaypcsolutions;
 
-import android.app.TabActivity;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.widget.TabHost;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class WPSPrototypeActivity extends TabActivity {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.graphics.PorterDuff;
+
+public class WPSPrototypeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        Resources res = getResources(); // Resource object to get Drawables
-        TabHost tabHost = getTabHost();  // The activity TabHost
-        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
-        Intent intent;  // Reusable Intent for each tab
-
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        intent = new Intent().setClass(this, WPSTab1.class);
-
-        // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("tab1").setIndicator("Graphical",
-                          res.getDrawable(R.drawable.wps_tab))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-
-        // Do the same for the other tabs
-        intent = new Intent().setClass(this, WPSTab2.class);
-        spec = tabHost.newTabSpec("tab2").setIndicator("Progress Bar",
-                          res.getDrawable(R.drawable.wps_tab))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-
-        tabHost.setCurrentTab(2);
-    }
+        setContentView(R.layout.progressbar);
+        
+        TextView status = (TextView) findViewById(R.id.status_indicator);
+        status.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+        
+        Button next = (Button) findViewById(R.id.btnVideoLib2);
+  	   next.setOnClickListener(new View.OnClickListener() {
+  	       public void onClick(View view) {
+  	           Intent myIntent = new Intent(view.getContext(), WPSVideoLibrary.class);
+  	           startActivityForResult(myIntent, 0);
+  	       }
+  	   });	
+  	   
+  	   Button dtTest = (Button) findViewById(R.id.button1);
+  	   dtTest.setOnClickListener(new View.OnClickListener() {
+  	       public void onClick(View view) {
+  	    	    
+  	    	  TimerTask task = new TimerTask()
+  	    	  {
+ 				@Override
+ 				public void run() {
+ 	      		
+ 	    	   			// Update Progress Bars
+ 	    	   			ProgressBar pbFeedPump = (ProgressBar) findViewById(R.id.progressBar1);
+ 	    	   			ProgressBar pbMediaFilter = (ProgressBar) findViewById(R.id.progressBar2);
+ 	    	   			ProgressBar pbCartridges = (ProgressBar) findViewById(R.id.progressBar3);
+ 	    	   			ProgressBar pbMotor = (ProgressBar) findViewById(R.id.progressBar4);
+ 	    	   			
+ 	    	   			
+ 	    	   			
+ 	    	   			// Save/Generate Data Table
+ 	    	   			saveToMemory();
+ 	    	   			
+ 	    	   			// Get Data Table
+ 	    	   			int[] data = loadFromMemory();
+ 	      		   
+ 	    	   			// Update Progress Bars
+ 	    	   			pbFeedPump.setProgress(data[0]);
+ 	    	   			pbMediaFilter.setProgress(data[1]);
+ 	    	   			pbCartridges.setProgress(data[2]);
+ 	    	   			pbMotor.setProgress(data[3]);		
+ 				} 
+  	    	  };
+  	    	  
+ 	    	  new Timer().scheduleAtFixedRate(task, 4000, 4000);
+  	    	  
+  	       }
+  	    	  
+  	   });	
+  	   
+ 	}
+ 	
+ 	public void saveToMemory() 
+ 	{
+ 		int feedPump = (int)(Math.random()*100);
+ 		int mediaFilter = (int)(Math.random()*100);
+ 		int cartridges = (int)(Math.random()*100);
+ 		int motor = (int)(Math.random()*100);
+ 		
+ 		try
+ 		{
+ 			FileOutputStream fos = openFileOutput ("DataTable.rpe", Context.MODE_PRIVATE);
+         	ObjectOutputStream oos = new ObjectOutputStream(fos);
+         	oos.writeBytes("*Data_Table*\n");
+         	
+         	//Data Table
+         	oos.writeBytes("FeedPump " + feedPump + "\n");
+         	oos.writeBytes("MediaFilter " + mediaFilter + "\n");
+         	oos.writeBytes("Cartridges " + cartridges + "\n");
+         	oos.writeBytes("Motor " + motor + "\n");
+         	
+         	oos.close();
+         	fos.close();
+         }
+         catch (IOException e)
+         {
+         	e.printStackTrace();
+         }
+ 		}
+ 	
+ 	
+ 	public int[] loadFromMemory()
+ 	{
+ 		
+ 		int data[] = new int[5];
+ 		data[0] = 75;
+ 		data[1] = 75;
+ 		data[2] = 75;
+ 		data[3] = 75;
+ 		
+ 		try
+ 		{
+ 			FileInputStream fin = openFileInput ("DataTable.rpe");
+ 			DataInputStream in = new DataInputStream(fin);
+ 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+ 		
+ 			String[] mySplit = new String[2];
+ 			String header;
+ 			String strLine;
+ 			int value;
+ 		
+ 			header = br.readLine(); //ignore this data
+ 		
+ 			//Feed Pump
+ 			strLine = br.readLine();
+ 			mySplit = strLine.split("\\s+");
+ 			strLine = mySplit[0];
+ 			value = Integer.parseInt(mySplit[1]);
+ 			data[0] = value;
+ 		
+ 			//Media Filter
+ 			strLine = br.readLine();
+ 			mySplit = strLine.split("\\s+");
+ 			strLine = mySplit[0];
+ 			value = Integer.parseInt(mySplit[1]);
+ 			data[1] = value;
+ 		
+ 			//Cartridges
+ 			strLine = br.readLine();
+ 			mySplit = strLine.split("\\s+");
+ 			strLine = mySplit[0];
+ 			value = Integer.parseInt(mySplit[1]);
+ 			data[2] = value;
+ 		
+ 			//Motor
+ 			strLine = br.readLine();
+ 			mySplit = strLine.split("\\s+");
+ 			strLine = mySplit[0];
+ 			value = Integer.parseInt(mySplit[1]);
+ 			data[3] = value;
+ 		
+ 			in.close();
+ 			
+ 			return data;
+ 		}	
+ 		catch (IOException e)
+ 		{
+ 			e.printStackTrace();
+ 		}
+ 		
+ 		return data;
+ 	}
+    
 }
