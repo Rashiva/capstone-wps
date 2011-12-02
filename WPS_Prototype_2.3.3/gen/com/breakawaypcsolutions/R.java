929cc738491072d3e7be9c1be3257f6338cfaac0
alex.neumann07 <alex.neumann07@gmail.com>
2011-12-02 05:54:57 +0000

    hopefully this works. the video is in there too.

--- WPS_Prototype_2.3.3/gen/com/breakawaypcsolutions/R.java
+++ WPS_Prototype_2.3.3/gen/com/breakawaypcsolutions/R.java
@@ -15,6 +15,7 @@ public final class R {
         public static final int ic_tab_artists_grey=0x7f020001;
         public static final int ic_tab_artists_white=0x7f020002;
         public static final int icon=0x7f020003;
+        public static final int wps_tab=0x7f020004;
     }
     public static final class id {
         public static final int btnVLA=0x7f050011;
@@ -39,8 +40,9 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int video=0x7f030001;
-        public static final int videolib=0x7f030002;
+        public static final int progressbar=0x7f030001;
+        public static final int video=0x7f030002;
+        public static final int videolib=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSVideo.java
+++ WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSVideo.java
@@ -2,15 +2,37 @@ package com.breakawaypcsolutions;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class WPSVideo extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.video);
+        
+        VideoView videoView = (VideoView) findViewById(R.id.videoView1);
+        /*MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setVideoPath("C:/User/Alex/Facebook.3gp");
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        videoView.start();*/
+        try{
+        MediaPlayer mp=new MediaPlayer();
+        SurfaceHolder holder = videoView.getHolder();
+        mp.setDataSource("C:/User/Alex/Facebook.3gp"); 
+        mp.setScreenOnWhilePlaying(true); 
+        mp.setDisplay(holder); 
+        mp.prepare(); 
+        mp.start();
+        }catch(Exception e){}
         
   	   Button next = (Button) findViewById(R.id.btnVidBack);
   	   next.setOnClickListener(new View.OnClickListener() {
