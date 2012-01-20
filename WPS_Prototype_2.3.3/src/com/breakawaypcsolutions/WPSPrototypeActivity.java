4e3e96b06e4a9ffb95e13fd7418738081f0f070b
ricky.padilla <ricky.padilla@gmail.com>
2012-01-20 16:46:16 +0000

    Updated the data table

--- WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSPrototypeActivity.java
+++ WPS_Prototype_2.3.3/src/com/breakawaypcsolutions/WPSPrototypeActivity.java
@@ -31,6 +31,7 @@ public class WPSPrototypeActivity extends Activity {
 	
 	ShapeDrawable pgDrawable1, pgDrawable2, pgDrawable3, pgDrawable4;
 	int sysStatus = 0; // 0 = Off, 1 = On, 2 = Override
+	int feedPumpStatus = 0; //0 = Out of Water, 1 = In Water
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -76,6 +77,16 @@ public class WPSPrototypeActivity extends Activity {
         	  pwr.getBackground().setColorFilter(0xffffff00, PorterDuff.Mode.MULTIPLY);
        	   	  pwr.setText("Power: OVERRIDE");
           }
+          
+          final ImageView feed_pump = (ImageView) findViewById(R.id.imageView1);
+          if(feedPumpStatus == 1)
+	   		{
+	   			feed_pump.setImageResource(R.drawable.green_check);
+	   		}
+	   		else
+	   		{
+	   			feed_pump.setImageResource(R.drawable.red_x);
+	   		}
         
   	   Button next = (Button) findViewById(R.id.btnVideoLib2);
   	   next.setOnClickListener(new View.OnClickListener() {
@@ -85,17 +96,15 @@ public class WPSPrototypeActivity extends Activity {
   	       }
   	   });
   	   
-  	   final ImageView feed_pump = (ImageView) findViewById(R.id.imageView1);
+  	   
   	   pwr.setOnClickListener(new View.OnClickListener() {
   	       public void onClick(View view) {
   	           if(sysStatus == 0) {
   	        	   sysStatus = 1;
-  	        	   feed_pump.setImageResource(R.drawable.green_check);
   	        	   pwr.getBackground().setColorFilter(0xff00ff00, PorterDuff.Mode.MULTIPLY);
   	        	   pwr.setText("Power: ON");
   	           } else {
   	        	   sysStatus = 0;
-  	        	   feed_pump.setImageResource(R.drawable.red_x);
   	        	   pwr.getBackground().setColorFilter(0xffff0000, PorterDuff.Mode.MULTIPLY);
 	        	   pwr.setText("Power: OFF");
   	           }
@@ -106,6 +115,8 @@ public class WPSPrototypeActivity extends Activity {
    	  {
 			@Override
 			public void run() {
+				
+				ImageView feed_pump = (ImageView) findViewById(R.id.imageView1);
     		
   	   			// Update Progress Bars
   	   			ProgressBar pb1 = (ProgressBar) findViewById(R.id.progressBar1);
@@ -117,24 +128,48 @@ public class WPSPrototypeActivity extends Activity {
   	   			
   	   			// Get Data Table
   	   			int[] data = loadFromMemory();
+  	   			
+  	   			sysStatus = data[0];
+  	   			
+	  	   		if(sysStatus == 0) {
+	          	  pwr.getBackground().setColorFilter(0xffff0000, PorterDuff.Mode.MULTIPLY);
+	         	   	  pwr.setText("Power: OFF");
+	            } else if (sysStatus == 1) {
+	          	  pwr.getBackground().setColorFilter(0xff00ff00, PorterDuff.Mode.MULTIPLY);
+	          	  pwr.setText("Power: ON");
+	            } else {
+	          	  pwr.getBackground().setColorFilter(0xffffff00, PorterDuff.Mode.MULTIPLY);
+	         	   	  pwr.setText("Power: OVERRIDE");
+	            }
+	  	   		
+	  	   		feedPumpStatus = data[1];
+	  	   		
+	  	   		if(feedPumpStatus == 1)
+	  	   		{
+	  	   			feed_pump.setImageResource(R.drawable.green_check);
+	  	   		}
+	  	   		else
+	  	   		{
+	  	   			feed_pump.setImageResource(R.drawable.red_x);
+	  	   		}
     		   
   	   			// Update Progress Bars
-  	   			if(data[0] < 50)
+  	   			if(data[2] < 50)
   	   				pgDrawable1.getPaint().setColor(0xffff0000); 
   	   			else
   	   				pgDrawable1.getPaint().setColor(0xff00ff00); 
-  	   			if(data[1] < 50)
+  	   			if(data[3] < 50)
   	   				pgDrawable2.getPaint().setColor(0xffff0000); 
   	   			else
   	   				pgDrawable2.getPaint().setColor(0xff00ff00); 
-  	   			if(data[2] < 50)
+  	   			if(data[4] < 50)
   	   				pgDrawable3.getPaint().setColor(0xffff0000); 
   	   			else
   	   				pgDrawable3.getPaint().setColor(0xff00ff00);
   	   			
-  	   			pb1.setProgress(data[0]);
-  	   			pb2.setProgress(data[1]);
-  	   			pb3.setProgress(data[2]);	
+  	   			pb1.setProgress(data[2]);
+  	   			pb2.setProgress(data[3]);
+  	   			pb3.setProgress(data[4]);	
 			} 
    	  };
    	  
@@ -144,10 +179,10 @@ public class WPSPrototypeActivity extends Activity {
  	
  	public void saveToMemory() 
  	{
- 		int feedPump = (int)(Math.random()*100);
- 		int mediaFilter = (int)(Math.random()*100);
- 		int cartridges = (int)(Math.random()*100);
- 		int motor = (int)(Math.random()*100);
+ 		int feedPump = (int)(Math.random());
+ 		int filters = (int)(Math.random()*100);
+ 		int roMembrane = (int)(Math.random()*100);
+ 		int waterPurity = (int)(Math.random()*100);
  		
  		try
  		{
@@ -156,10 +191,11 @@ public class WPSPrototypeActivity extends Activity {
          	oos.writeBytes("*Data_Table*\n");
          	
          	//Data Table
-         	oos.writeBytes("FeedPump " + feedPump + "\n");
-         	oos.writeBytes("MediaFilter " + mediaFilter + "\n");
-         	oos.writeBytes("Cartridges " + cartridges + "\n");
-         	oos.writeBytes("Motor " + motor + "\n");
+         	oos.writeBytes("Power " + sysStatus + "\n"); //Power: 0 = Off, 1 = On, 2 = Override
+         	oos.writeBytes("FeedPump " + feedPump + "\n"); //Feed Pump in water: 0=Not in water, 1=In water
+         	oos.writeBytes("Filters " + filters + "\n"); //Filters: Percentage value 0 - 100
+         	oos.writeBytes("ROMembrane " + roMembrane + "\n"); //R/O Membrane: Percentage value 0 - 100
+         	oos.writeBytes("WaterPurity " + waterPurity + "\n"); //Water Purity: Percentage value 0 - 100
          	
          	oos.close();
          	fos.close();
@@ -175,10 +211,10 @@ public class WPSPrototypeActivity extends Activity {
  	{
  		
  		int data[] = new int[5];
- 		data[0] = 75;
- 		data[1] = 75;
- 		data[2] = 75;
- 		data[3] = 75;
+ 		//data[0] = 75;
+ 		//data[1] = 75;
+ 		//data[2] = 75;
+ 		//data[3] = 75;
  		
  		try
  		{
@@ -193,33 +229,40 @@ public class WPSPrototypeActivity extends Activity {
  		
  			header = br.readLine(); //ignore this data
  		
- 			//Feed Pump
+ 			//Power
  			strLine = br.readLine();
  			mySplit = strLine.split("\\s+");
  			strLine = mySplit[0];
  			value = Integer.parseInt(mySplit[1]);
  			data[0] = value;
  		
- 			//Media Filter
+ 			//Feed Pump
  			strLine = br.readLine();
  			mySplit = strLine.split("\\s+");
  			strLine = mySplit[0];
  			value = Integer.parseInt(mySplit[1]);
  			data[1] = value;
  		
- 			//Cartridges
+ 			//Filters
  			strLine = br.readLine();
  			mySplit = strLine.split("\\s+");
  			strLine = mySplit[0];
  			value = Integer.parseInt(mySplit[1]);
  			data[2] = value;
  		
- 			//Motor
+ 			//R/O Membrane
  			strLine = br.readLine();
  			mySplit = strLine.split("\\s+");
  			strLine = mySplit[0];
  			value = Integer.parseInt(mySplit[1]);
  			data[3] = value;
+ 			
+ 			//Water Purity
+ 			strLine = br.readLine();
+ 			mySplit = strLine.split("\\s+");
+ 			strLine = mySplit[0];
+ 			value = Integer.parseInt(mySplit[1]);
+ 			data[4] = value;
  		
  			in.close();
  			
